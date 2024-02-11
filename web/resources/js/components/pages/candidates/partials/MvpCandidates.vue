35598257713e72d135f09eec47e3d7052766ee93
pitylee <pitylee@gmail.com>
2024-02-11 23:14:09 +0200

    Colored skills labels, hiding unwanted candidates, make use of MvpCandidates

--- web/resources/js/components/pages/candidates/partials/MvpCandidates.vue
+++ web/resources/js/components/pages/candidates/partials/MvpCandidates.vue
@@ -6,7 +6,7 @@
       </div>
       <ul role="list"
         class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-y-16 gap-x-8 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">
-        <li v-for="person in people" :key="person.name">
+        <li v-for="person in people.filter((entity) => entity.isMvp === true)" :key="person.name">
           <img class="mx-auto h-24 w-24 rounded-full" :src="person.imageUrl" alt="" />
           <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{{ person.name }}</h3>
           <p class="text-sm leading-6 text-gray-600">{{ person.role }}</p>
